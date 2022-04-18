badf8bcf01525b7b7b3ea0293b55e82e3e2a2e9c
fujialong-1 <3079754535@qq.com>
2022-04-18 21:45:39 +0800

    add new file

---
+++ src/main/java/thread/DeadLockDemo.java
@@ -0,0 +1,54 @@
+package thread;
+
+/**
+ * 死锁示范及jstack使用
+ * jstack用于生成java虚拟机当前时刻的线程快照。
+ * @Author: Jialong Fu
+ * @Date: 2022/4/18 21:33
+ */
+public class DeadLockDemo {
+
+    private static Object lockA = new Object();
+    private static Object lockB = new Object();
+
+    private static void startThreadA() {
+        Thread aThread = new Thread() {
+
+            @Override
+            public void run() {
+                synchronized (lockA) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                    }
+                    synchronized (lockB) {
+                    }
+                }
+            }
+        };
+        aThread.start();
+    }
+
+    private static void startThreadB() {
+        Thread bThread = new Thread() {
+            @Override
+            public void run() {
+                synchronized (lockB) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                    }
+                    synchronized (lockA) {
+                    }
+                }
+            }
+        };
+        bThread.start();
+    }
+
+    public static void main(String[] args) {
+        startThreadA();
+        startThreadB();
+    }
+
+}
